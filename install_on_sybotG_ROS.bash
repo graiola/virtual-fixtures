35c678fcd738fbf328556e982f2f2565aac0724b
Gennaro RAIOLA <gennaro.raiola@gmail.com>
2016-06-29 10:21:17 +0200

    Use scp power :D

--- install_on_sybotG_ROS.bash
+++ install_on_sybotG_ROS.bash
@@ -1,2 +1,3 @@
 # ROS VERSION
-scp -r /home/sybot/ros_catkin_ws/install_isolated sybot@sybotG:/home/sybot/ros_catkin_ws/
+scp -r /home/sybot/ros_catkin_ws/install_isolated/{lib,include} sybot@sybotG:/home/sybot/ros_catkin_ws/install_isolated/
+
