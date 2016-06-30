7119dc729bdf378377f0f22093fd6f90ec6f2810
Gennaro RAIOLA <gennaro.raiola@gmail.com>
2016-06-30 17:56:28 +0200

    Funny tests

--- mechanism_manager/test/test_mechanism_manager.cpp
+++ mechanism_manager/test/test_mechanism_manager.cpp
@@ -104,6 +104,8 @@ TEST(MechanismManagerTest, UpdateMethodRawVectors)
   START_REAL_TIME_CRITICAL_CODE();
   EXPECT_NO_THROW(mm->Update(&rob_pos_std[0],&rob_vel_std[0],dt,&f_out_std[0]));
   END_REAL_TIME_CRITICAL_CODE();
+
+  delete mm;
 }
 
 TEST(MechanismManagerTest, GetVmPositionAndVelocity)
@@ -189,8 +191,6 @@ TEST(MechanismManagerTest, InsertVmUpdateGetPositionAndVelocityDelete) // Most a
   // Delete Note: this is async, so it could happen that there is nothing to delete because Insert is still going on
   EXPECT_NO_THROW(mm->DeleteVM(0));
 
-  getchar(); // To avoid that delete hits before the end of DeleteVM
-
   delete mm;
 }
 
@@ -205,8 +205,6 @@ TEST(MechanismManagerTest, LoopUpdate)
 
   MechanismManager mm;
   EXPECT_NO_THROW(mm.InsertVM(model_name3));
-
-  getchar();
 
   int pos_dim = mm.GetPositionDim();
 
@@ -227,17 +225,17 @@ TEST(MechanismManagerTest, LoopUpdate)
 
   double scale, phase;
 
-  int n_steps = 100;
+  int n_steps = 100000;
   for (int i=0;i<n_steps;i++)
   {
 
-      START_REAL_TIME_CRITICAL_CODE();
+      //START_REAL_TIME_CRITICAL_CODE();
       EXPECT_NO_THROW(mm.Update(rob_pos,rob_vel,dt,f_out));
       EXPECT_NO_THROW(mm.GetVmPosition(0,pos));
       EXPECT_NO_THROW(mm.GetVmVelocity(0,vel));
       EXPECT_NO_THROW(scale = mm.GetScale(0));
       EXPECT_NO_THROW(phase = mm.GetPhase(0));
-      END_REAL_TIME_CRITICAL_CODE();
+      //END_REAL_TIME_CRITICAL_CODE();
 
       /*if(i == 7)
       {
