42cd488f2534967d0b3cafe9c149c2bc23110910
gennaro <gennaro.raiola@gmail.com>
2016-08-29 00:38:55 +0200

    toolbox license part 2

--- toolbox/include/toolbox/toolbox.h
+++ toolbox/include/toolbox/toolbox.h
@@ -1,3 +1,26 @@
+/**
+ * @file   toolbox.h
+ * @brief  Meta header file.
+ * @author Gennaro Raiola
+ *
+ * This file is part of virtual-fixtures, a set of libraries and programs to create
+ * and interact with a library of virtual guides.
+ * Copyright (C) 2014-2016 Gennaro Raiola, ENSTA-ParisTech
+ *
+ * virtual-fixtures is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * virtual-fixtures is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with virtual-fixtures.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #ifndef TOOLBOX_H
 #define TOOLBOX_H
 
