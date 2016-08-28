a8aad363701b6d33052daf79e8b70909e835a307
gennaro <gennaro.raiola@gmail.com>
2016-08-29 00:04:36 +0200

    license toolbox

--- toolbox/include/toolbox/ros.h
+++ toolbox/include/toolbox/ros.h
@@ -11,6 +11,7 @@
 #include <ros/ros.h>
 #include <ros/package.h>
 #ifdef USE_ROS_RT_PUBLISHER
+  #include <std_msgs/Float64.h>
   #include <std_msgs/Float64MultiArray.h>
   #include <geometry_msgs/PoseStamped.h>
   #include <geometry_msgs/WrenchStamped.h>
@@ -56,6 +57,7 @@ class RosNode
         RosNode()
         {
           init_ = false;
+          ros_nh_ptr_ = NULL;
         }
 
         void Init(std::string ros_node_name)
@@ -65,7 +67,8 @@ class RosNode
           char* argv[] = {arg0, 0};
           ros::init(argc, argv, ros_node_name,ros::init_options::NoSigintHandler);
           free (arg0);
-          if(ros::master::check()){
+          if(ros::master::check())
+          {
               ros_nh_ptr_ = new ros::NodeHandle(ros_node_name);
           }
           else
@@ -79,7 +82,7 @@ class RosNode
         ~RosNode(){if(ros_nh_ptr_!=NULL && init_ == true){ros_nh_ptr_->shutdown(); delete ros_nh_ptr_;}}
 
         ros::NodeHandle& GetNode()
-        {
+        { 
           if(init_ == true)
             return *ros_nh_ptr_;
           else
@@ -87,6 +90,29 @@ class RosNode
             std::string err("RosNode not initialized");
             throw std::runtime_error(err);
           }
+        }
+
+        bool Reset()
+        {
+          if(init_ == true)
+          {
+            ros_nh_ptr_->shutdown();
+            delete ros_nh_ptr_;
+            ros_nh_ptr_ = NULL;
+            init_ = false;
+            return true;
+          }
+          else
+          {
+            std::string err("RosNode not initialized");
+            throw std::runtime_error(err);
+            return false;
+          }
+        }
+
+        bool InitDone()
+        {
+            return init_;
         }
 
     protected:
@@ -97,6 +123,7 @@ class RosNode
 
 #ifdef USE_ROS_RT_PUBLISHER
 
+template <typename T>
 class RealTimePublisherVector
 {
     public:
@@ -111,7 +138,8 @@ class RealTimePublisherVector
 
         }
         /** Publish the topic. */
-        inline void Publish(const Eigen::Ref<const Eigen::VectorXd>& in)
+        //inline void Publish(const Eigen::Ref<const Eigen::VectorXd>& in)
+        inline void Publish(const T& in)
         {
             if(pub_ptr_ && pub_ptr_->trylock())
             {
@@ -164,7 +192,38 @@ class RealTimePublisherVector
     private:
         typedef realtime_tools::RealtimePublisher<std_msgs::Float64MultiArray> rt_publisher_t;
         std::string topic_name_;
-        int msg_size_;
+        boost::shared_ptr<rt_publisher_t > pub_ptr_;
+};
+
+
+class RealTimePublisherScalar
+{
+    public:
+
+        /** Initialize the real time publisher. */
+        RealTimePublisherScalar(const ros::NodeHandle& ros_nh, const std::string topic_name)
+        {
+            // Checks
+            assert(topic_name.size() > 0);
+            topic_name_ = topic_name;
+            pub_ptr_.reset(new rt_publisher_t(ros_nh,topic_name,10));
+
+        }
+        /** Publish the topic. */
+        inline void Publish(const double& in)
+        {
+            if(pub_ptr_ && pub_ptr_->trylock())
+            {
+                pub_ptr_->msg_.data = in;
+                pub_ptr_->unlockAndPublish();
+            }
+        }
+
+        inline std::string getTopic(){return topic_name_;}
+
+    private:
+        typedef realtime_tools::RealtimePublisher<std_msgs::Float64> rt_publisher_t;
+        std::string topic_name_;
         boost::shared_ptr<rt_publisher_t > pub_ptr_;
 };
 
@@ -173,23 +232,35 @@ class RealTimePublishers
 {
     public:
 
-        RealTimePublishers(){};
-
         // Add a RealTimePublisher already created
-        void AddPublisher(boost::shared_ptr<RealTimePublisher_t> pub_ptr, Eigen::VectorXd* vector_ptr)
+        void AddPublisher(boost::shared_ptr<RealTimePublisher_t> pub_ptr, double* data_ptr)
         {
             assert(pub_ptr!=false);
-            // Put it into the map with his friend
-            map_[pub_ptr->getTopic()] = std::make_pair(vector_ptr,pub_ptr);
+            // Put it into the map with his friends
+            map_[pub_ptr->getTopic()] = std::make_pair(data_ptr,pub_ptr);
         }
 
         // Add a new fresh RealTimePublisher
-        void AddPublisher(const ros::NodeHandle& ros_nh, const std::string topic_name, Eigen::VectorXd* vector_ptr)
+        void AddPublisher(const ros::NodeHandle& ros_nh, const std::string topic_name, double* data_ptr)
         {
             boost::shared_ptr<RealTimePublisher_t> pub_ptr = boost::make_shared<RealTimePublisher_t>(ros_nh,topic_name);
             assert(pub_ptr!=false);
-            // Put it into the map with his friend
-            map_[pub_ptr->getTopic()] = std::make_pair(vector_ptr,pub_ptr);
+            // Put it into the map with his friends
+            map_[pub_ptr->getTopic()] = std::make_pair(data_ptr,pub_ptr);
+        }
+
+        // Publish!
+        void PublishAll()
+        {
+            for(pubs_map_it_t iterator = map_.begin(); iterator != map_.end(); iterator++)
+                iterator->second.second->Publish(*iterator->second.first);
+        }
+ /// Code used before when it was needed to change dynamically the size of the vectors to publish
+/*
+        // Resize a specific publisher
+        void Resize(const int dim, const std::string topic_name)
+        {
+            map_[topic_name].second->Resize(dim);
         }
 
         // Remove an element from all the publishers
@@ -212,16 +283,11 @@ class RealTimePublishers
             for(pubs_map_it_t iterator = map_.begin(); iterator != map_.end(); iterator++)
                 iterator->second.second->PushBackEmpty();
         }
+*/
 
-        // Publish!
-        void PublishAll()
-        {
-            for(pubs_map_it_t iterator = map_.begin(); iterator != map_.end(); iterator++)
-                iterator->second.second->Publish(*iterator->second.first);
-        }
     private:
 
-        typedef std::map<std::string,std::pair<Eigen::VectorXd*,boost::shared_ptr<RealTimePublisher_t> > > pubs_map_t;
+        typedef std::map<std::string,std::pair<double*,boost::shared_ptr<RealTimePublisher_t > > > pubs_map_t;
         typedef typename pubs_map_t::iterator pubs_map_it_t;
 
         pubs_map_t map_;
--- toolbox/include/toolbox/utilities.h
+++ toolbox/include/toolbox/utilities.h
@@ -1,3 +1,26 @@
+/**
+ * @file   utilities.h
+ * @brief  Utility functions and classes.
+ * @author Gennaro Raiola
+ *
+ * This file is part of virtual-fixtures, a set of libraries and programs to create
+ * and interact with a library of virtual guides.
+ * Copyright (C) 2014-2016 Gennaro Raiola, ENSTA-ParisTech
+ *
+ * virtual-fixtures is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * virtual-fixtures is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with virtual-fixtures.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #ifndef UTILITIES_H
 #define UTILITIES_H
 
@@ -89,7 +112,10 @@ class AsyncThread
         }
         inline void AddHandler(funct_t f)
         {
+            //if(f_.empty())
             f_ = f;
+            //else
+            //    std::cerr << "Callback already defined" << std::endl;
         }
         inline void Trigger()
         {
@@ -107,6 +133,7 @@ class AsyncThread
                         trigger_ = false;
                         callback_ =  boost::thread(f_);
                         callback_.join();
+                        //f_.clear();
                     }
                     else
                         std::cerr << "No Callback function" << std::endl;
